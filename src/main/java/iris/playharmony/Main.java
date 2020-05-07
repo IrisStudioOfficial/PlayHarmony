e8c6a993936420844fbe6d06cfde7413a3808e78
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-05-07 18:01:50 +0100

    Initial commit 7-5-2020

--- src/main/java/iris/playharmony/Main.java
+++ src/main/java/iris/playharmony/Main.java
@@ -5,6 +5,7 @@ import static javafx.application.Application.launch;
 public class Main {
 
     public static void main(String[] args) {
+        
         launch(PlayHarmony.class, args);
     }
 }
