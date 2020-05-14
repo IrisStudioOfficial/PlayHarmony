0dba2774ad701a09c65479c5be2021423cda1e99
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-05-14 19:14:20 +0100

    Merged

--- src/main/java/iris/playharmony/Main.java
+++ src/main/java/iris/playharmony/Main.java
@@ -2,7 +2,6 @@ package iris.playharmony;
 import static javafx.application.Application.launch;
 
 public class Main {
-
     public static void main(String[] args) {
         launch(PlayHarmony.class, args);
     }
